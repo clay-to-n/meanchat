0e2b5b8cca4608208c70e7270946cc7acc5c439b
clay-to-n <cbrand@usc.edu>
2015-01-05 17:35:15 -0800

    Messages now display with username of the sender

--- app.js
+++ app.js
@@ -9,10 +9,11 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
   console.log('a user connected');
   socket.on('disconnect', function(){
-    console.log('user disconnected');
+    console.log('a user disconnected');
   });
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  socket.on('chat message', function(message){
+    console.log(message);
+    io.emit('chat message', message);
   });
 });
 
